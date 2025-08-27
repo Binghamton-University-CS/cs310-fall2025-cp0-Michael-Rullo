cffd03c8ea15381810c391a6f8b60270d6550964
mrullo <mrullo@cs.binghamton.edu>
2025-08-27 19:04:27 -0400

    original

--- zoo.cpp
+++ zoo.cpp
@@ -1,20 +1,35 @@
 #include <iostream>
 #include <stdlib.h>
-#include "Animal.h"
+
+using namespace std;
+
+class Animal {
+   public:
+      Animal(string speciesName, unsigned int discoveryYear) {
+         species = speciesName;
+         year_discovered = discoveryYear;
+      }
+
+      Animal() : species(""), year_discovered(0) {};
+
+      void display() {
+         cout << species << " [" << year_discovered << "]" << endl;
+      }
+
+   private:
+      string species = "";
+      unsigned int year_discovered = 0;
+};
 
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
-   Animal animal3("Siberian Tiger", 1844);
-   AnimalsInZoo *containment = new AnimalsInZoo(animal3);
 
-   
    delete animal1;
    animal1 = new Animal("Snow Leopard", 1777);
-   
-   animal3.display();
+
    animal2.display();
    animal1->display();
 
    delete animal1;
-};
+}
