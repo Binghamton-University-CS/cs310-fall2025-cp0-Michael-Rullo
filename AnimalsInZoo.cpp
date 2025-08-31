a0a1ceeadbf4d1196c03ae5584b4bcb3c6d99e62
mrullo <mrullo@cs.binghamton.edu>
2025-08-31 15:38:49 -0400

    debug and fix

--- AnimalsInZoo.cpp
+++ AnimalsInZoo.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include <stlib.h>
+#include <stdlib.h>
 #include "AnimalsInZoo.h"
 #include "Animal.h"
 
 void AnimalsInZoo::display() {
   std::cout << "Number of Animals: " << numAnimals << std::endl;
   if (numAnimals > 0) {
-    Animal.display();
+    animal.display();
   }
 }
--- AnimalsInZoo.h
+++ AnimalsInZoo.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include "Animal.h"
 
 class AnimalsInZoo {
  public:
