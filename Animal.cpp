a75940ad4963bdeb0d620d315cd3ac349fbb1a3a
mrullo <mrullo@cs.binghamton.edu>
2025-08-31 15:06:32 -0400

    forgot to add files

---
+++ Animal.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Animal.h"
+
+using namespace std;
+
+void display(String species, unsigned int year_discovered) {
+  cout << species << " [" << year_discovered << "] " << endl;
+}
